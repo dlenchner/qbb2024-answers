bf95763c50145fb88bdeaa8e3ef0bd6df7b94355
Daniel Lenchner <dlenchn1@jh.edu>
2024-09-04 15:44:42 -0400

    Afternoon exercise 1 - grep.py

---
+++ day2-homework/grep.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sys
+
+my_file = open( sys.argv[2] )
+
+for line in my_file:
+    line = line.rstrip("\n")
+    if sys.argv[1] in line:
+        print( line )
+
+
+my_file.close()
+
