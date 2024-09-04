a65bfc235e54363c27085c7e9e0dcff4c1d19a61
Daniel Lenchner <dlenchn1@jh.edu>
2024-09-03 23:15:13 -0400

    Updates to the lunch exercises to improve clarity

--- d1-lunch/d1-morning-exercises.R
+++ d1-lunch/d1-morning-exercises.R
@@ -1,9 +1,18 @@
+#Q2
+
 library(tidyverse)
+
+
+#Q3
 
 df <- read_tsv("~/Data/GTEx/GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt") #assign the text file data to the variable "df"
 
 df <- df %>%
   mutate( SUBJECT=str_extract( SAMPID, "[^-]+-[^-]+" ), .before=1 ) #generate a SUBJECT column
+
+
+
+#Q4
 
 #Arrange the SUBJECTs in ascending and descending order by "sample_number"
 df %>% #search within the dataset "df"
@@ -15,6 +24,8 @@ df %>% #search within the dataset "df"
 #Two SUBJECTS with most samples: K-562 (217 samples) and GTEX-NPJ8 (72 samples)
 #Two SUBJECTS with fewest samples: GTEX-1JMI6 (1 sample) and GTEX-1PAR6 (1 sample)
 
+
+#Q5
 
 #Arrange the SMTSDs (tissue types) in ascending and descending order by "sample_number"
 df %>%
@@ -28,6 +39,8 @@ df %>%
 #Two SMTSDs with fewest samples: Kidney-Medulla (4 samples) and Cervix-Ectocervix (9 samples)
   #WHY: Difficult to collect
 
+
+#Q6
 
 df_npj8 <- filter(df, SUBJECT == "GTEX-NPJ8") #filter/retain data from SUBJECT "GTEX-NPJ8" within the dataset "df" and store the new dataset as "df_npj8"
 
@@ -44,21 +57,38 @@ view(df_npj8)
 
 #Within the "Whole Blood" samples, what is different between the samples: Nucleic acid collection method and sequencing method
 
+
+
+#Q7
+
 #Filter out any SMATSSCR value of "NA" from the "df" dataset
-df %>%
+df_no_na <- df %>%
   filter( !is.na(SMATSSCR) )
 
+df_no_na_mean <- df_no_na %>%
+  group_by(SUBJECT) %>%
+  summarize(mean_sm = mean(SMATSSCR)) %>%
+  arrange(mean_sm)
 
+df_mean_dist <- df_no_na_mean %>%
+  group_by(mean_sm) %>%
+  summarize( count = n() ) %>%
+  arrange(mean_sm)
 
+ggplot(data = df_no_na_mean, 
+       mapping = aes(x = mean_sm)) +
+  geom_histogram(binwidth = 0.1) +
+  labs(
+    title = "Autolysis Score Distribution",
+    x = "Mean Autolysis Score",
+    y = "Number of Subjects"
+  )
 
+ggsave(filename = "~/qbb2024-answers/d1-lunch/d1-morning_SMATSSCR-dist.pdf")
 
-
-
-
-
-
-
-
+#15 subjects have a mean SMATSSCR of 0
+#The distribution of mean scores can be used to determine the most common mean score amongst the subbjects
+#A histogram can display the distribution of mean scores
 
 
 
