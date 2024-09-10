0630f631936de1e3dbe1eeab58ddd147cb11d51b
Daniel Lenchner <dlenchn1@jh.edu>
2024-09-09 21:58:17 -0400

    Updated the morning assignment scripts to include comments

--- d4-morning/d4-morning.R
+++ d4-morning/d4-morning.R
@@ -1,23 +1,24 @@
 library(tidyverse)
 
+# read the file created with my python code into R
 df <- read_delim("~/qbb2024-answers/d4-morning/genes_tissues_expression.tsv")
 
 view(df)
 
 
-
+# create a new column with the combined Tissue and Gene_ID. Assign to the variable df_tissue_geneid
 df_tissue_geneid <- df %>%
   dplyr::mutate(Tissue_Gene=paste0(Tissue, " ", Gene_ID))
 
 view(df_tissue_geneid)
 
-
+# create a new column where the log, base 2, is taken for each expression value (which is given a pseudocount of +1). Assign to the variable df_log2
 df_log2 <- df_tissue_geneid %>%
   dplyr::mutate(log2_expression = log2(Expression + 1.0))
 
 view(df_log2)
 
-
+# plot the log-normalized expression levels for each tissue/gene pair using a violin plot
 ggplot(data = df_log2, mapping = aes(x = Tissue_Gene, y = log2_expression)) + 
   geom_violin() +
   coord_flip() +
@@ -27,6 +28,7 @@ ggplot(data = df_log2, mapping = aes(x = Tissue_Gene, y = log2_expression)) +
     y = "Gene Expression (log2)"
   )
 
+# save the plot as a png
 ggsave(filename = "~/qbb2024-answers/d4-morning/d4-morning-plot.png")
 
 # Given the high expression and high tissue specificity observed for these 
--- d4-morning/d4-morning.py
+++ d4-morning/d4-morning.py
@@ -4,7 +4,9 @@ import sys
 
 import numpy
 
-# get target file name and open the file
+# Exercise 1 - maintained the original dictionary that uses the (gene, tissue) tuple as keys
+
+# get target file, name the file, and open the file
 filename = sys.argv[1]
 fs = open(filename, mode = 'r')
 
@@ -20,13 +22,13 @@ for line in fs:
 fs.close()
 
 
-# print(relevant_samples)
+# print(relevant_samples)                       # test the above code
 
 
 
+# Exercise 2
 
-
-# get metadata file name and open the file
+# get metadata file, name the file, and open the file
 filename = sys.argv[2]
 fs = open(filename, mode = 'r')
 
@@ -41,15 +43,18 @@ for line in fs:
     fields = line.rstrip("\n").split("\t")      # split line into fields
     key = fields[6]                             # create key from tissue
     value = fields[0]                           # create value from sampleID
-    tissue_samples.setdefault( key, [] )        # initialize dictionary from key with list to hold samples
-    tissue_samples[key].append(value)        
+    tissue_samples.setdefault( key, [] )        # initialize dictionary from key with list to hold sampleIDs
+    tissue_samples[key].append(value)           # using the key created, add the sampleID to the value list in the dictionary
              
 fs.close()
 
-# print(tissue_samples)
+# print(tissue_samples)                         # test the above code                
 
 
-# get raw data file name and open the file
+# Exercise 3-5 - these steps were covered by Mike in lecture and compiled into this streamlined method
+
+
+# get raw data file, name the file, and open the file
 filename = sys.argv[3]
 fs = open(filename, mode = 'r')
 
@@ -57,58 +62,64 @@ fs = open(filename, mode = 'r')
 fs.readline()
 fs.readline()
 
-# take the third line in the file (the column names), strip the new line character and split by tabs. Assign to a new variable "header"
+# take the third line in the file (the column names), strip the new line character and split by tabs. Assign to a new variable header
 header = fs.readline().rstrip("\n").split("\t") 
 
 header = header[2:]
 
+# print(header)                                 # test the above code
 
-# print(header)
-
+# create an empty dictionary to hold indices associated with particular tissues in the raw data file
 tissue_columns = {}
 
-for tissue, samples in tissue_samples.items():      # the items function returns both the keys and values from the tissue_samples dictionary and stores the keys as the variable tissue and the values as the variable samples
+for tissue, samples in tissue_samples.items():      # use the items function to return both the keys and values from the tissue_samples dictionary. Store the keys as the variable tissue and the values as the variable samples
     tissue_columns.setdefault(tissue, [])           # in the empty tissue_columns dictionary, any time a new tissue is encountered, give it a value of a blank list
     for sample in samples:                          # step through each sample in the list of samples associated with the given tissue
-        if sample in header:
-            position = header.index(sample)
-            tissue_columns[tissue].append(position)
+        if sample in header:                        # check if each sample is in the header created in line 63. When that condition is satisfied -->
+            position = header.index(sample)         # save the index as the variable position
+            tissue_columns[tissue].append(position) # append the position (index) to the list created (line 71) for that given tissue
 
-fs.close()
+# fs.close()
 
-# print(tissue_columns)
+# # print(tissue_columns)                             # test the above code
 
 
 
-# get target file name and open the file
-filename = sys.argv[3]
-fs = open(filename, mode = 'r')
+# # get raw data file, name the file, and open the file
+# filename = sys.argv[3]
+# fs = open(filename, mode = 'r')
 
-fs.readline()
-fs.readline()
-fs.readline()
+# # skip the first three lines
+# fs.readline()
+# fs.readline()
+# fs.readline()                                       # NOTE: lines 77-90 are unnecessary. The code runs fine (and in fact, a bit faster) if the program doesn't need to close and reopen the raw data file. I'm choosing to leave this extra code in place to show my initial logic.
 
+
+# Exercise 6-7 - I combined these steps given the nested for loops shown below
+
+# create the header for the resulting tsv file
 print("Expression", "Gene_ID", "Tissue")
 
+# step through the file
 i = 0
 for line in fs:
-    fields = line.rstrip("\n").split("\t")      # split line into fields
-    gene_ID = fields[0]
-    for gene, tissue in relevant_samples.keys():
-        if gene_ID == gene:
-            expression_values = []
-            expression_values.append(fields[2:])
-            expression_values_array = numpy.array(expression_values, dtype = float)
-            sample_indices = tissue_columns.get(tissue)
-            # print(tissue)
-            # print(sample_indices)
-            # print(expression_values_array)
-            expression_val_interest = list(expression_values_array[0][sample_indices])
-            # print(expression_val_interest)
-            for index in expression_val_interest:
-                print(expression_val_interest[i], gene, tissue)
-                i += 1
-            i = 0
+    fields = line.rstrip("\n").split("\t")                                          # split line into fields
+    gene_ID = fields[0]                                                             # assign the value in the first column to the variable gene_ID
+    for gene, tissue in relevant_samples.keys():                                    # use the keys function to return the keys from the relevant_samples dictionary. Store the gene in each key as the variable gene and the tissue in each key as the variable tissue
+        if gene_ID == gene:                                                         # check if the gene_ID from the raw data file matches the gene from the relevant samples dictionary. When that condition is satisfied -->
+            expression_values = []                                                  # create an empty list for the expression_values associated with the gene
+            expression_values.append(fields[2:])                                    # append the expression values associated with the gene in the raw data file to the expression_values list
+            expression_values_array = numpy.array(expression_values, dtype = float) # turn the expression_values list into a numpy array, specifying the data type as float
+            sample_indices = tissue_columns.get(tissue)                             # use the get function on the tissue_columns dictionary to retrieve the indices (values) of interest for the tissue (key) associated with the gene being examined
+            # print(tissue)                                                         # test the above code
+            # print(sample_indices)                                                 # test the above code
+            # print(expression_values_array)                                        # test the above code
+            expression_val_interest = list(expression_values_array[0][sample_indices])  # from the expression_values_array, extract/list the expression values corresponding to the sample indices retrieved from the tissue_columns dictionary
+            # print(expression_val_interest)                                        # test the above code
+            for index in expression_val_interest:                                   # step through each index in the newly generated expression_val_interest list
+                print(expression_val_interest[i], gene, tissue)                     # beneath the header printed in line 94, print the expression value at index i, the gene being examined, and the paired tissue
+                i += 1                                                              # add 1 to i to continue stepping through the indices of expression_val_interest
+            i = 0                                                                   # reset i to 0
                  
 fs.close()
 
