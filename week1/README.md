2f4a0b5684db98e7567f00ed6eedcb54c413da7e
Daniel Lenchner <dlenchn1@jh.edu>
2024-09-13 21:56:02 -0400

    Exercise 1.5 - Simulating 10x coverage of the genome

--- week1/README.md
+++ week1/README.md
@@ -29,9 +29,18 @@ Step 1.3
 Step 1.4
 
     1.  Using the grep function in Unix on the genome_coverage_3x.txt file I made, I found that 49,450 bp were not covered by any reads (roughly 5% of the genome)
-    2.  The number of genes lacking coverage (~50,000) matches the expectation based on the Poisson distribution plotted with the histogram.
+    2.  The number of positions lacking coverage (~50,000) matches the expectation based on the Poisson distribution plotted with the histogram.
         For the most part, the normal distribution fits the data really well (the curve for the normal distribution overlaps the histogram bars almost perfectly). However, the normal distribution overestimates the number of samples with zero coverage to be ~70,000.
 
+
+
+Step 1.5
+
+    Number of reads: 100,000 reads are needed to achieve 10x coverage of a 1 Mbp genome with 100 bp reads
+
+    1.  Using the grep function in Unix on the genome_coverage_10x.txt file I made, I found that only 23 bp were not covered by any reads
+    2.  Again, the number of positions lacking coverage matches the expectation based on the Poisson distribution pretty well, though it is not quite as exact as   was observed in the 3x coverage simulation.
+        In this instance, the normal distribution matches the Poisson distribution a bit more closely than was observed previously. However, both distributions stray from the data more than in the 3x coverage example.
 
 
 
--- week1/week1.py
+++ week1/week1.py
@@ -9,14 +9,14 @@ import numpy
 # assign variables to each of the factors needed to calculate the number of reads (genomoe size, the size of each read, and the coverage we're looking for)
 genome_size = 1000000       # 1 Mbp = 1,000,000 bp
 read_size = 100             # 100 bp per read
-coverage = 3               # 3x coverage of the genome
+coverage = 10               # 10x coverage of the genome    -   CHANGED TO 10X FOREXERCISE 1.5
 
 # perform the calculation for the number of reads needed to cover a 1 Mbp
 read_number = (genome_size * coverage) / read_size
 
 # print(read_number)            # test the above code to calculate the number of reads needed
 
-# Answer: 30,000 reads are needed to achieve 3x coverage of a 1 Mbp genome with 100 bp reads
+# Answer: 100,000 reads are needed to achieve 10x coverage of a 1 Mbp genome with 100 bp reads
 
 
 # Exercise 1.2
@@ -27,7 +27,7 @@ genome_coverage = numpy.zeros(genome_size, int)
 
 
 # randomly simulate sequencing the genome and count the number of times each position is covered
-# for each of the 30,000 iterations (reads) calculated above
+# for each of the 100,000 iterations (reads) calculated above
 for i in range(int(read_number)):
     start_position = numpy.random.randint(0, genome_size - read_size + 1)           # randomly assign a starting position that could fall anywhere from the 0th position to the 999,900th position of the genome_coverage array (because python is upper limit exclusionary) - genome position 1 to 999,901 
     end_position = start_position + read_size                                       # assign an ending position by adding the read size (100 bp) to the starting position assigned above
