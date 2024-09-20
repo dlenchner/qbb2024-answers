2317bfddc43832992435511e78d0f3acbc6aeaea
Daniel Lenchner <dlenchn1@jh.edu>
2024-09-19 22:59:24 -0400

    Updates to exercise 2 to eliminate the duplicate edges

--- week1/week1_exercise2.py
+++ week1/week1_exercise2.py
@@ -10,17 +10,18 @@ reads = ['ATTCA', 'ATTGA', 'CATTG', 'CTTAT', 'GATTG', 'TATTT', 'TCATT', 'TCTTA',
 # set variable k = 3 (each k-mer has a length of 3 nucleotides)
 k = 3
 
-# create an empty list 
-graph = []
+# create an empty DICTIONARY 
+graph = {}
 
 # generate the edges for the de Bruijn graph and add them to the empty graph list
 # step through each of the 5-mer genome reads in the reads list
 for read in reads:
-    for i in range(len(read) - k):                          # within each read, iterate at indices 0 and 1 -> range(len(read) - k) = range(len(read) - 3) = range(5-3) = range(2) = [0, 1]
-        kmer1 = read[i : i + k]                             # assign kmer1 to the three nucleotides from index i to index i + k (because python is upper limit exclusionary, adding k = 3 to i will generate a kmer1 that is 3 nt long)
-        kmer2 = read[i + 1 : i + 1 + k]                     # assign kmer2 to the three nucletoides shifted over 1 index from those in kmer1
+    for i in range(len(read) - k):                              # within each read, iterate at indices 0 and 1 -> range(len(read) - k) = range(len(read) - 3) = range(5-3) = range(2) = [0, 1]
+        kmer1 = read[i : i + k]                                 # assign kmer1 to the three nucleotides from index i to index i + k (because python is upper limit exclusionary, adding k = 3 to i will generate a kmer1 that is 3 nt long)
+        kmer2 = read[i + 1 : i + 1 + k]                         # assign kmer2 to the three nucletoides shifted over 1 index from those in kmer1
         # print(kmer1 + " -> " + kmer2)            # test the variable assignments above
-        graph.append(kmer1 + " -> " + kmer2)                # append the edge generated with kmer1 and kmer2 to the graph list
+        graph.setdefault(kmer1 + " -> " + kmer2, 0)             # initialize the graph dictionary with each new edge as the key and "0" as the value
+        graph[kmer1 + " -> " + kmer2] += 1                      # add 1 to the value associated with the edge/key
         
 # print(graph)              # test the above code
 
@@ -34,7 +35,7 @@ print(header)
 for edge in graph:
     print(edge)                                             # print the edge
 
-
+# generate a footer for the resulting file
 footer = "}"
 print(footer)
 
